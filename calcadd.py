d4c01e1649bc60516d9c143d3ac31d297e7204d4
codecollie <75228676+codecollie@users.noreply.github.com>
2020-12-02 18:26:02 -0700

    Add files via upload

--- calcadd.py
+++ calcadd.py
@@ -1,7 +1,8 @@
 # file: calcadd.py
 
-num1 = input("Enter a gay ass number: ")
-num2 = input("Enter another gay ass number: ")
+num1 = input("Enter a ghey ass number: ")
+num2 = input("Enter another ghey ass number: ")
 result = float(num1) + float(num2)
 
+print("\nEpstein still didn't kill himself.")
 print(result)
