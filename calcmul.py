368e64f00742cdf8142f1354747ec4dd3dc9c4f6
codecollie <75228676+codecollie@users.noreply.github.com>
2020-12-08 18:23:33 -0700

    Add files via upload

--- calcmul.py
+++ calcmul.py
@@ -1,8 +1,8 @@
-# file python/calcadd.py
+# file: python/calcmul.py
 
 num1 = input("enter a ghey ass number: ")
 num2 = input("enter another ghey ass number: ")
-result = float(num1) + float(num2)
+result = float(num1) * float(num2)
 
 print("\nepstein still didn't kill himself.")
 print(result)
--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,3 @@
 # file: /python/helloworld.py
 
 print("epstein didn't kill himself.")
-
